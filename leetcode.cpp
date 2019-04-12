08f7a7d7a46edd907491a105e5d120d285ce4809
lionzhnn <37626634+lionzhnn@users.noreply.github.com>
2019-04-12 19:21:46 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -7,9 +7,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode *temp1,*temp2;
         ListNode *temp=new ListNode(0);
         temp1=l1;
@@ -43,5 +41,38 @@ public:
         temp=temp->next;
         delete temp3;
         return temp;
-    }
-};
+}
+//26. Remove Duplicates from Sorted Array
+int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) 
+            return 0;
+        if(nums.size() ==1)
+            return 1;
+        auto baseNum=nums[0];
+        auto curNum=nums[1];
+        int indexForward=1;
+        int length=1;
+        for(int j=1;j<nums.size();j++)
+        {
+            if(curNum==baseNum)
+            {
+                //前移一位
+                for(int i=indexForward;i!=(nums.size()-1);i++)
+                {
+                    nums[i]=nums[i+1];
+                }
+                //把重复的数值放到最后
+                nums[nums.size()-1]=curNum;
+                curNum=nums[indexForward];
+            }
+            if(curNum>baseNum)
+            {
+                baseNum=curNum;
+                indexForward=indexForward+1;
+                length=length+1;
+                if(indexForward<nums.size())
+                    curNum=nums[indexForward];
+            }
+        }
+        return length;
+}
