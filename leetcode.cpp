b36cc42f1cac498df9bda1b1aafd6452cc7fe139
lionzhnn <37626634+lionzhnn@users.noreply.github.com>
2019-04-15 18:27:41 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -66,3 +66,27 @@ int removeElement(vector<int>& nums, int val) {
         }
         return lenght;        
 }
+//28. Implement strStr()
+int strStr(string haystack, string needle) {
+        if(needle.size()==0)
+            return 0;
+        if(haystack.size()<needle.size())
+            return -1;
+        for(int i=0;i<=(haystack.size()-needle.size());i++)
+        {
+            if(haystack[i]==needle[0])
+            {
+                int j;
+                for(j=1;j<needle.size();j++)
+                {
+                    if(haystack[i+j]!=needle[j])
+                    {
+                        break;
+                    }
+                }
+                if(j==needle.size())
+                    return i;
+            }
+        }
+        return -1;        
+}
